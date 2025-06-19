30a5a37768cbe56f5feff704fcae9fd637b35a57
Tian Sheng Ryan Tan <tiatan@c1r8s5.42singapore.sg>
2025-06-19 21:37:48 +0800

    feat(ex03): halfway through

---
+++ ex03/ClapTrap.cpp
@@ -0,0 +1,111 @@
+#include "ClapTrap.hpp"
+#include <iostream>
+
+ClapTrap::ClapTrap()
+    : name("No Name"), hp(10), ep(10), ad(0)
+{
+    std::cout << "ClapTrap " << name << " has been constructed" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string& name)
+    : name(name), hp(10), ep(10), ad(0)
+{
+    std::cout << "ClapTrap " << name << " has been constructed" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string& name, unsigned int hp, unsigned int ep, unsigned int ad)
+    : name(name), hp(hp), ep(ep), ad(ad)
+{
+    std::cout << "ClapTrap " << name << " has been constructed" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& other)
+    : name(other.name), hp(other.hp), ep(other.ep), ad(other.ad)
+{
+    std::cout << "Copy of ClapTrap " << other.name << " constructed" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other)
+{
+    std::cout << "Copy assignment of ClapTrap " << other.name << " called" << std::endl;
+    if (this != &other)
+    {
+        name = other.name;
+        hp = other.hp;
+        ep = other.ep;
+        ad = other.ad;
+    }
+    return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "ClapTrap " << name << " has been destroyed" << std::endl;
+}
+
+const std::string& ClapTrap::getName() const
+{
+    return (name);
+}
+
+unsigned int ClapTrap::getHp() const
+{
+    return (hp);
+}
+
+unsigned int ClapTrap::getEp() const
+{
+    return (ep);
+}
+
+unsigned int ClapTrap::getAd() const
+{
+    return (ad);
+}
+
+void ClapTrap::setName(const std::string& name)
+{
+    this->name = name;
+}
+
+void ClapTrap::setHp(const unsigned int amount)
+{
+    this->hp = amount;
+}
+
+void ClapTrap::setEp(const unsigned int amount)
+{
+    this->ep = amount;
+}
+
+void ClapTrap::setAd(const unsigned int amount)
+{
+    this->ad = amount;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+    if (!hp || !ep)
+        return ;
+    std::cout << "ClapTrap " << name << " attacks " << target
+        << ", causing " << ad << " points of damage!" << std::endl
+        << "Remaining energy points: " << ep << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if (amount > hp)
+        amount = hp;
+    hp -= amount;
+    std::cout << "ClapTrap " << name << " has taken " << amount << " points of damage!" << std::endl
+        << "Remaining health points: " << hp << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (!hp || !ep)
+        return ;
+    hp += amount;
+    std::cout << "ClapTrap " << name << " has been repaired for " << amount << " points of health!" << std::endl
+        << "Remaining energy points: " << ep << std::endl << "Total health points: " << hp << std::endl;
+}
---
+++ ex03/ScavTrap.cpp
@@ -0,0 +1,49 @@
+#include "ScavTrap.hpp"
+#include <iostream>
+
+ScavTrap::ScavTrap()
+    : ClapTrap()
+{
+    std::cout << "ScavTrap " << this->getName() << " has been constructed" << std::endl;
+    setHp(100);
+    setEp(50);
+    setAd(20);
+}
+
+ScavTrap::ScavTrap(const std::string& name)
+    : ClapTrap(name, 100, 50, 20)
+{
+    std::cout << "ScavTrap " << this->getName() << " has been constructed" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& other)
+    : ClapTrap(other)
+{
+    std::cout << "Copy of ScavTrap " << other.getName() << " constructed" << std::endl;
+}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap& other)
+{
+    std::cout << "Copy assignment of ScavTrap " << other.getName() << " called" << std::endl;
+    if (this != &other)
+    {
+        ClapTrap::operator=(other);
+    }
+    return (*this);
+}
+
+ScavTrap::~ScavTrap()
+{
+    std::cout << "ScavTrap " << this->getName() << " has been destroyed" << std::endl;
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+    std::cout << "ScavTrap " << this->getName() << " attacks " << target
+        << ", causing " << this->getAd() << " points of damage!!" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+    std::cout << "ScavTrap " << this->getName() << " is now in Gatekeeper mode." << std::endl;
+}
